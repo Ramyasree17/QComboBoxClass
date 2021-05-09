6db176206997c0099806ec082c5ebf018b69ef62
Ramyasree17 <79324300+Ramyasree17@users.noreply.github.com>
2021-05-09 16:45:33 +0530

    Add files via upload

---
+++ QComboBoxClass/main.cpp
@@ -0,0 +1,55 @@
+#include "widget.h"
+
+#include <QApplication>
+#include<QFile>
+#include<QTextStream>
+QString readTextFile(QString path)
+{
+
+    QFile file(path);
+
+    if(file.open(QIODevice::ReadOnly|QIODevice::Text))
+
+    {
+
+        QTextStream in(&file);
+
+        return in.readAll();
+
+    }
+
+    return " ";
+
+}
+
+int main(int argc, char *argv[])
+
+
+
+{
+
+    QApplication a(argc, argv);
+
+
+
+    QString css=readTextFile(":/style/sheet.txt");//Copy the path of text document[shown in steps video]
+
+
+
+    if(css.length()>0)
+
+
+
+    {
+
+      a.setStyleSheet(css);
+
+}
+
+     Widget w;
+
+      w.show();
+
+     return a.exec();
+
+}
---
+++ QComboBoxClass/widget.cpp
@@ -0,0 +1,62 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include<QDebug>
+#include<QIcon>
+#include<QStringList>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+     QStringList list;
+
+     list <<"Fruits"<<"Apple"<<"Orange"<<"papaya"<<"JackFruit";
+
+     ui->comboBox->setStyleSheet("background-color: rgb(0, 0, 0) ; color: rgb(0, 255, 255);");
+
+        ui->comboBox->setEditable(true);
+
+        for(int i=0;i<list.size();i++)
+
+        {
+
+        ui->comboBox->addItem(QIcon("C:/Users/swath/OneDrive/Desktop/icons.jfif"),list.at(i));
+
+    }
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::on_SetValuepushButton_clicked()
+{
+    ui->comboBox->setCurrentIndex(2);
+}
+
+void Widget::on_GetValuepushButton_clicked()
+{
+    qDebug()<<"The combo box currently has"<<QString::number(ui->comboBox->count())<<"Items.They are";
+
+        for(int i=0;i<ui->comboBox->count();i++)
+
+        {
+
+            qDebug()<<"Index"<<QString::number(i)<<":"<<ui->comboBox->itemText(i);
+
+        }
+}
+
+void Widget::on_CaptureValuepushButton_clicked()
+{
+      ui->comboBox->currentText();
+
+      qDebug()<<"The currently choosen item is"<<(ui->comboBox->currentText());
+
+     qDebug()<<"The Index is:"<<ui->comboBox->currentIndex();
+
+
+}
